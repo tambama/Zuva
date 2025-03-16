ed23934c351ca48cc4a0d2e74e062f35bc4bac1d
Peniel Tambama <tambama.peniel@outlook.com>
2025-03-16 09:00:29 +0200

    Initial

---
+++ Zuva/Zuva.cs
@@ -0,0 +1,20 @@
+using System;
+using cAlgo.API;
+using cAlgo.API.Collections;
+using cAlgo.API.Indicators;
+using cAlgo.API.Internals;
+
+namespace cAlgo
+{
+    [Indicator(AccessRights = AccessRights.None)]
+    public class Zuva : Indicator
+    {
+        protected override void Initialize()
+        {
+        }
+
+        public override void Calculate(int index)
+        {
+        }
+    }
+}
