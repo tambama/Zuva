90953d76f267472772f2aaf4d2f78912db3756ab
Peniel Tambama <tambama.peniel@outlook.com>
2025-04-03 20:19:23 +0200

    hapana apa

--- Zuva/obj/Debug/net6.0/Zuva.AssemblyInfo.cs
+++ Zuva/obj/Debug/net6.0/Zuva.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zuva")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+789c7b65110da98ae2df5d47e0713a718c852fde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bbf1a8c105728088418306b300259653dd2a8e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zuva")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zuva")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
