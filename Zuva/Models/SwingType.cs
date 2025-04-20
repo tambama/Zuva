c3cd9bfe856bd4126f715e02c0a4e94d68bd380e
Peniel Tambama <tambama.peniel@outlook.com>
2025-04-20 17:29:53 +0200

    update

--- Zuva/Models/SwingType.cs
+++ Zuva/Models/SwingType.cs
@@ -5,10 +5,5 @@ namespace Zuva.Models;
 public enum SwingType
 {
     [Description("High")] H,
-    [Description("Low")] L,
-    [Description("Higher High")] HH,
-    [Description("Higher Low")] HL,
-    [Description("Lower High")] LH,
-    [Description("Lower Low")] LL,
-    [Description("Inducement")] IND
+    [Description("Low")] L
 }
