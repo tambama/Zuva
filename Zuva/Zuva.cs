def13afa4ccbfd55b0792aade5e7490f0e487979
Peniel Tambama <tambama.peniel@outlook.com>
2025-04-20 14:59:26 +0200

    stuff

--- Zuva/Zuva.cs
+++ Zuva/Zuva.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using cAlgo.API;
 using cAlgo.API.Indicators;
 using cAlgo.API.Internals;
@@ -8,39 +10,60 @@ namespace cAlgo
     [Indicator(IsOverlay = true, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
     public class Zuva : Indicator
     {
+        [Parameter("Show Swing Points", DefaultValue = true)]
+        public bool ShowSwingPoints { get; set; }
+
+        [Parameter("Show Fair Value Gaps", DefaultValue = true)]
+        public bool ShowFairValueGaps { get; set; }
+
+        [Parameter("Bullish FVG Color", DefaultValue = "DeepSkyBlue")]
+        public string BullishFvgColor { get; set; }
+
+        [Parameter("Bearish FVG Color", DefaultValue = "Magenta")]
+        public string BearishFvgColor { get; set; }
+
+        [Parameter("FVG Opacity", DefaultValue = 50, MinValue = 0, MaxValue = 100)]
+        public int FvgOpacity { get; set; }
+
         [Output("Swing High", Color = Colors.Green, PlotType = PlotType.Points, Thickness = 4)]
         public IndicatorDataSeries SwingHighs { get; set; }
 
         [Output("Swing Low", Color = Colors.Red, PlotType = PlotType.Points, Thickness = 4)]
         public IndicatorDataSeries SwingLows { get; set; }
 
-        private SwingPointDetector _detector;
+        private SwingPointDetector _swingDetector;
+        private PDArrayDetector _pdDetector;
         private int _lastProcessedIndex = -1;
-
+        private List<FvgDrawing> _fvgDrawings;
+        
         protected override void Initialize()
         {
             // Initialize swing detector
-            _detector = new SwingPointDetector(Bars);
+            _swingDetector = new SwingPointDetector(Bars);
+            
+            // Initialize PD Array detector
+            _pdDetector = new PDArrayDetector(Bars);
+            
+            // Initialize FVG drawings list
+            _fvgDrawings = new List<FvgDrawing>();
+            
+            // Process historical data
+            for (int i = 2; i < Bars.Count - 1; i++)
+            {
+                Calculate(i);
+            }
         }
 
         public override void Calculate(int index)
         {
-            // Need at least 1 bar to calculate
-            if (index <= 0)
+            // Need at least 2 bars to calculate
+            if (index < 2)
                 return;
                 
-            // Check if this is a new bar or still the current bar forming
-            bool isNewBar = index > _lastProcessedIndex;
-            bool isLastBar = index == Bars.Count - 1;
-            
-            // Only process closed bars (any bar except the last one) or if this is a historical bar we haven't processed yet
-            if (!isLastBar || (isLastBar && isNewBar && index > 1))
+            // Process swing points
+            if (ShowSwingPoints)
             {
-                // Update the last processed index
-                _lastProcessedIndex = index;
-                
-                // Calculate swing points
-                SwingPoint swingPoint = _detector.DetectSwingPoint(index);
+                SwingPoint swingPoint = _swingDetector.DetectSwingPoint(index);
                 
                 if (swingPoint.HasSwingHigh)
                 {
@@ -63,6 +86,82 @@ namespace cAlgo
                     SwingLows[swingPoint.PreviousSwingLowIndex] = double.NaN;
                 }
             }
+            
+            // Process Fair Value Gaps
+            if (ShowFairValueGaps)
+            {
+                ProcessFairValueGaps(index);
+            }
+        }
+        
+        private void ProcessFairValueGaps(int index)
+        {
+            // Detect new FVG
+            FairValueGap fvg = _pdDetector.DetectFairValueGap(index);
+            
+            if (fvg != null)
+            {
+                // Get color based on FVG type (bullish or bearish)
+                Color fvgColor = fvg.IsBullish 
+                    ? ParseColorWithOpacity(BullishFvgColor) 
+                    : ParseColorWithOpacity(BearishFvgColor);
+                
+                // Create rectangle to visualize the FVG
+                string objectName = $"FVG_{fvg.StartIndex}_{index}_{DateTime.Now.Ticks}";
+                var rectangle = Chart.DrawRectangle(
+                    objectName, 
+                    Bars.OpenTimes[fvg.StartIndex], 
+                    fvg.UpperBound, 
+                    Bars.OpenTimes[index + 10 > Bars.Count - 1 ? Bars.Count - 1 : index + 10], // Extend rectangle
+                    fvg.LowerBound, 
+                    Color.Red);
+                
+                // Add to the tracking list
+                _fvgDrawings.Add(new FvgDrawing { 
+                    FairValueGap = fvg, 
+                    Rectangle = rectangle, 
+                    Name = objectName 
+                });
+                
+                // Add debugging
+                Print($"Created FVG at index {index}: {fvg.IsBullish} Upper:{fvg.UpperBound} Lower:{fvg.LowerBound}");
+            }
+            
+            // Update FVG status
+            _pdDetector.UpdatePDArrayStatus(index);
+            
+            // Remove inactive FVGs
+            List<FvgDrawing> toRemove = new List<FvgDrawing>();
+            
+            foreach (var drawing in _fvgDrawings)
+            {
+                if (!drawing.FairValueGap.IsActive)
+                {
+                    Chart.RemoveObject(drawing.Name);
+                    toRemove.Add(drawing);
+                    Print($"Removed FVG: {drawing.Name}");
+                }
+            }
+            
+            foreach (var item in toRemove)
+            {
+                _fvgDrawings.Remove(item);
+            }
+        }
+        
+        private Color ParseColorWithOpacity(string colorName)
+        {
+            Color baseColor = Color.FromName(colorName);
+            int alpha = (int)(255 * (FvgOpacity / 100.0));
+            return Color.FromArgb(alpha, baseColor.R, baseColor.G, baseColor.B);
+        }
+        
+        // Helper class to track FVG drawings
+        private class FvgDrawing
+        {
+            public FairValueGap FairValueGap { get; set; }
+            public ChartRectangle Rectangle { get; set; }
+            public string Name { get; set; }
         }
     }
 }
--- Zuva/obj/Debug/net6.0/Zuva.AssemblyInfo.cs
+++ Zuva/obj/Debug/net6.0/Zuva.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zuva")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bbf1a8c105728088418306b300259653dd2a8e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90953d76f267472772f2aaf4d2f78912db3756ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zuva")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zuva")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
