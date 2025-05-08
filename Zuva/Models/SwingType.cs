629d2d4c14079a944c6fb90bf2eecbe83c92935b
Peniel Tambama <tambama.peniel@outlook.com>
2025-05-08 21:06:52 +0200

    market structure

--- Zuva/Models/SwingType.cs
+++ Zuva/Models/SwingType.cs
@@ -7,7 +7,5 @@ public enum SwingType
     [Description("High")] H,
     [Description("Low")] L,
     [Description("Higher High")] HH,
-    [Description("Lower High")] LH,
-    [Description("Higher Low")] HL,
     [Description("Lower Low")] LL
 }
