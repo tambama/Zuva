fd8bda4f5fbc061635f3d24a520f1b55daf706a0
Peniel Tambama <tambama.peniel@outlook.com>
2025-05-11 08:19:01 +0200

    before liquidity sweep

--- Zuva/obj/Debug/net6.0/Zuva.AssemblyInfo.cs
+++ Zuva/obj/Debug/net6.0/Zuva.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zuva")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df78e9373dddd4a87349736e7be410b09aadcdfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d25a81528b7399457b0c371d115dd32ad03121f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zuva")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zuva")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
