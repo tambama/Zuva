769f429ae6020a857969b7ab630bc8f763946fda
Peniel Tambama <tambama.peniel@outlook.com>
2025-05-19 10:39:32 +0200

    Telegram Service

---
+++ Zuva/Services/TelegramService.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Net;
+using Telegram.Bot;
+
+namespace Mwenje.Services;
+
+public class TelegramService
+{
+    public TelegramService()
+    {
+        ServicePointManager.Expect100Continue = true;
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+    }
+
+    public string SendTelegram(string chatId, string token, string telegramMessage)
+    {
+        string reply = string.Empty;
+        long id = Convert.ToInt64(chatId);
+
+        try
+        {
+            var bot = new TelegramBotClient(token);
+            bot.SendTextMessageAsync(id, telegramMessage);
+            reply = "SUCCESS";
+        }
+        catch (Exception ex)
+        {
+            reply = "ERROR: " + ex.Message;
+        }
+
+        return reply;
+    }
+}
--- Zuva/obj/Debug/net6.0/Zuva.AssemblyInfo.cs
+++ Zuva/obj/Debug/net6.0/Zuva.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zuva")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18fcf43e6e923056e8b798380c3927afa0783864")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0de46969cab50c6685652dffa040c482ec7cf5e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zuva")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zuva")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
